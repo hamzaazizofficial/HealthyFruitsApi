8edb3914084639e9028936d4b98aa2dfc70c7a5e
Hamza Aziz <37170062+hamzaazizofficial@users.noreply.github.com>
2025-02-12 12:30:21 +0530

    Update mainRoute.js

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -2,12 +2,12 @@ import express from "express";
 import fruits from "../data/fruits.js";
 import vegetables from "../data/vegetables.js";
 import allItems from "../data/allItems.js";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-// Get __dirname equivalent for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// // Get __dirname equivalent for ES Modules
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 
 const mainRouter = express.Router();
