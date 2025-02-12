8799b9579f442853265eec0d0ea2064a3c63a32f
Hamza Aziz <37170062+hamzaazizofficial@users.noreply.github.com>
2025-02-12 12:11:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+// import express from "express";
+// import mainRouter from "./routes/mainRoute.js";
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.set('view engine', 'ejs');
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+// app.use(mainRouter);
+
+// app.listen(PORT, () => {
+//     console.log(`Server running at ${PORT}`);
+// });
+
 import express from "express";
+import ejs from "ejs";
 import mainRouter from "./routes/mainRoute.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get __dirname equivalent for ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// app.set('view engine', 'ejs');
+// Set 'views' directory for any views 
+// being rendered res.render()
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine as EJS
+app.engine('html', ejs.renderFile);
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
