4a0e452bb25a094942e039897e4fe7619397ab11
Hamza Aziz <37170062+hamzaazizofficial@users.noreply.github.com>
2025-02-12 12:17:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,6 @@
 // app.listen(PORT, () => {
 //     console.log(`Server running at ${PORT}`);
 // });
-
 import express from "express";
 import ejs from "ejs";
 import mainRouter from "./routes/mainRoute.js";
@@ -30,11 +29,15 @@ const PORT = process.env.PORT || 3000;
 // app.set('view engine', 'ejs');
 // Set 'views' directory for any views 
 // being rendered res.render()
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 
-// Set view engine as EJS
-app.engine('html', ejs.renderFile);
-app.set('view engine', 'ejs');
+// // Set view engine as EJS
+// app.engine('html', ejs.renderFile);
+// app.set('view engine', 'ejs');
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Ensure correct path
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(mainRouter);
